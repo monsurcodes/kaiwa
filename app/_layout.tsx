eab1adc40f9ebe3d3006b700a86c1d4ee2daf7be
monsurcodes <monsurcodes@gmail.com>
2026-04-10 15:15:07 +0530

    feat: wrap Stack component in View and add StatusBar for improved layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,26 @@
 import "./../global.css";
 
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
 
 export default function RootLayout() {
    return (
-      <Stack>
-         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-         <Stack.Screen name="anime/[id]" options={{ headerShown: false }} />
-         <Stack.Screen name="manga/[id]" options={{ headerShown: false }} />
-         <Stack.Screen name="auth/login" options={{ headerShown: false }} />
-      </Stack>
+      <View className="flex-1 bg-primary">
+         <StatusBar style="light" />
+         <Stack
+            screenOptions={{
+               contentStyle: {
+                  backgroundColor: "#030014",
+               },
+               animation: "fade_from_bottom",
+            }}
+         >
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="anime/[id]" options={{ headerShown: false }} />
+            <Stack.Screen name="manga/[id]" options={{ headerShown: false }} />
+            <Stack.Screen name="auth/login" options={{ headerShown: false }} />
+         </Stack>
+      </View>
    );
 }
