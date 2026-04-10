6110690cb2ac87767dbe5ebce0bb7ec2aee6b149
monsurcodes <monsurcodes@gmail.com>
2026-04-10 14:05:58 +0530

    feat: implement root layout with navigation screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,5 +3,12 @@ import "./../global.css";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-   return <Stack />;
+   return (
+      <Stack>
+         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+         <Stack.Screen name="anime/[id]" options={{ headerShown: false }} />
+         <Stack.Screen name="manga/[id]" options={{ headerShown: false }} />
+         <Stack.Screen name="auth/login" options={{ headerShown: false }} />
+      </Stack>
+   );
 }
