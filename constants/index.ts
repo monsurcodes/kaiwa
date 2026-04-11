02bb6fe40762932763a925869c9988eeac7b3e1e
monsurcodes <monsurcodes@gmail.com>
2026-04-11 23:32:25 +0530

    feat: add Anilist client ID, redirect URI, and API URL constants

---
+++ constants/index.ts
@@ -0,0 +1,4 @@
+export const ANILIST_CLIENT_ID = process.env.EXPO_PUBLIC_ANILIST_CLIENT_ID;
+export const ANILIST_REDIRECT_URI = process.env.EXPO_PUBLIC_ANILIST_REDIRECT_URI;
+
+export const ANILIST_API_URL = "https://graphql.anilist.co";
