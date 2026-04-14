cb554cbdf1d1a3da52049099f5299c74b498c72a
monsurcodes <monsurcodes@gmail.com>
2026-04-14 15:41:10 +0530

    feat: add storage keys for authentication management

--- constants/index.ts
+++ constants/index.ts
@@ -2,3 +2,9 @@ export const ANILIST_CLIENT_ID = process.env.EXPO_PUBLIC_ANILIST_CLIENT_ID;
 export const ANILIST_REDIRECT_URI = process.env.EXPO_PUBLIC_ANILIST_REDIRECT_URI;
 
 export const ANILIST_API_URL = "https://graphql.anilist.co";
+
+export const STORAGE_KEYS = {
+   AUTH_TOKEN: "auth.token",
+   AUTH_EXPIRES_AT: "auth.expiresAt",
+   USER: "auth.user",
+} as const;
