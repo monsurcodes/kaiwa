f2f0d60e563bcf063d7658b6fbdc9657c153ddc5
monsurcodes <monsurcodes@gmail.com>
2026-04-14 18:24:20 +0530

    fix: add ANILIST_CLIENT_SECRET to constants for authentication

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,5 @@
 export const ANILIST_CLIENT_ID = process.env.EXPO_PUBLIC_ANILIST_CLIENT_ID;
+export const ANILIST_CLIENT_SECRET = process.env.EXPO_PUBLIC_ANILIST_CLIENT_SECRET;
 export const ANILIST_REDIRECT_URI = process.env.EXPO_PUBLIC_ANILIST_REDIRECT_URI;
 
 export const ANILIST_API_URL = "https://graphql.anilist.co";
