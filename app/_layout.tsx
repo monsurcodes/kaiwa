2e49674235ee809d4e98b078b8c69b565ab7a520
monsurcodes <monsurcodes@gmail.com>
2026-04-14 15:41:39 +0530

    refactor: replace View with GestureHandlerRootView and SafeAreaView for improved layout handling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,25 +2,36 @@ import "./../global.css";
 
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Provider } from "urql";
+
+import { client } from "@/lib/graphql/client";
 
 export default function RootLayout() {
    return (
-      <View className="flex-1 bg-primary">
-         <StatusBar style="light" />
-         <Stack
-            screenOptions={{
-               contentStyle: {
-                  backgroundColor: "#030014",
-               },
-               animation: "fade_from_bottom",
-            }}
-         >
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="anime/[id]" options={{ headerShown: false }} />
-            <Stack.Screen name="manga/[id]" options={{ headerShown: false }} />
-            <Stack.Screen name="auth/login" options={{ headerShown: false }} />
-         </Stack>
-      </View>
+      <GestureHandlerRootView style={{ flex: 1, backgroundColor: "#030014" }}>
+         <SafeAreaProvider>
+            <Provider value={client}>
+               <StatusBar style="light" />
+               <SafeAreaView edges={["top"]} style={{ flex: 1, backgroundColor: "#030014" }}>
+                  <Stack
+                     screenOptions={{
+                        contentStyle: {
+                           backgroundColor: "#030014",
+                           padding: 1,
+                        },
+                        animation: "fade_from_bottom",
+                     }}
+                  >
+                     <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                     <Stack.Screen name="anime/[id]" options={{ headerShown: false }} />
+                     <Stack.Screen name="manga/[id]" options={{ headerShown: false }} />
+                     <Stack.Screen name="auth/login" options={{ headerShown: false }} />
+                  </Stack>
+               </SafeAreaView>
+            </Provider>
+         </SafeAreaProvider>
+      </GestureHandlerRootView>
    );
 }
