aa8171d0e4100e2e3d84f6b8f925a059d3ba6bda
monsurcodes <monsurcodes@gmail.com>
2026-04-15 20:33:30 +0530

    feat: add GraphQL query and TypeScript interface for trending manga

---
+++ lib/graphql/queries/getTrendingManga.ts
@@ -0,0 +1,33 @@
+import { gql } from "urql";
+
+export const GetTrendingMangaQuery = gql`
+   query GetTrendingManga($page: Int = 1) {
+      Page(page: $page, perPage: 10) {
+         pageInfo {
+            hasNextPage
+            currentPage
+         }
+         media(type: MANGA, sort: TRENDING_DESC) {
+            id
+            title {
+               english
+               romaji
+            }
+            averageScore
+            favourites
+            coverImage {
+               large
+            }
+            bannerImage
+            description
+            genres
+            # studios(isMain: true) {
+            #    nodes {
+            #       id
+            #       name
+            #    }
+            # }
+         }
+      }
+   }
+`;
