e436253b922580b8b5183b0f3c3b8f1ca892313f
monsurcodes <monsurcodes@gmail.com>
2026-04-17 20:33:56 +0530

    feat: add popular anime section and improve loading indicators in Index component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,55 @@
 import { FlashList } from "@shopify/flash-list";
 import React from "react";
-import { Dimensions, ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, Dimensions, ScrollView, Text, View } from "react-native";
 import { useQuery } from "urql";
 
 import TrendingMediaCard from "@/components/TrendingMediaCard";
+import { GetPopularAnimeQuery } from "@/lib/graphql/queries/getPopularAnime";
 import { GetTrendingAnimeQuery } from "@/lib/graphql/queries/getTrendingAnime";
 import { GetTrendingMangaQuery } from "@/lib/graphql/queries/getTrendingManga";
+import { PopularAnimeInterface } from "@/types/popularAnimeInterface";
 import { type TrendingAnimeInterface } from "@/types/trendingAnimeInterface";
 import { type TrendingMangaInterface } from "@/types/trendingMangaInterface";
 const { width } = Dimensions.get("window");
 const CARD_WIDTH = width * 0.8;
 
 const Index = () => {
+   // trending anime
    const [animeResult] = useQuery<TrendingAnimeInterface["data"]>({
       query: GetTrendingAnimeQuery,
    });
 
-   const { data: animeList, fetching: animeFetching, error: animeError } = animeResult; // , error: animeError
-   // console.log("Trending Anime Data:", animeList);
+   const { data: animeList, fetching: animeFetching, error: animeError } = animeResult;
 
-   if (animeError) console.error("Trending Anime Error:", animeError);
+   // popular anime
+   const [popularAnimeResult] = useQuery<PopularAnimeInterface["data"]>({
+      query: GetPopularAnimeQuery,
+   });
 
+   const {
+      data: popularAnimeList,
+      fetching: popularAnimeFetching,
+      error: popularAnimeError,
+   } = popularAnimeResult;
+
+   // trending manga
    const [mangaResult] = useQuery<TrendingMangaInterface["data"]>({
       query: GetTrendingMangaQuery,
    });
 
-   const { data: mangaList, fetching: mangaFetching, error: mangaError } = mangaResult; // , error: mangaError
-   // console.log("Trending Manga Data:", mangaList);
+   const { data: mangaList, fetching: mangaFetching, error: mangaError } = mangaResult;
 
-   if (mangaError) console.error("Trending Manga Error:", mangaError);
+   // TODO: handle errors properly
+   if (animeError) console.error("Error fetching trending anime:", animeError);
+   if (popularAnimeError) console.error("Error fetching popular anime:", popularAnimeError);
+   if (mangaError) console.error("Error fetching trending manga:", mangaError);
+
+   if (animeFetching || popularAnimeFetching || mangaFetching)
+      return (
+         <View className="flex-1 items-center justify-center">
+            <ActivityIndicator size={30} />
+         </View>
+      );
 
    return (
       <View className="flex-1">
@@ -37,8 +58,10 @@ const Index = () => {
             showsVerticalScrollIndicator={false}
             showsHorizontalScrollIndicator={false}
          >
-            <Text className="mb-2 mt-6 text-xl font-semibold text-white">Trending Anime</Text>
-            {animeFetching && <Text className="text-white">Loading...</Text>}
+            {/* Trending Anime */}
+            {animeList && (
+               <Text className="mb-2 mt-6 text-xl font-semibold text-white">Trending Anime</Text>
+            )}
             {animeList && (
                <FlashList
                   style={{ height: 350, width: "100%" }}
@@ -62,12 +85,44 @@ const Index = () => {
                   horizontal
                ></FlashList>
             )}
-            <Text className="mb-2 mt-6 text-xl font-semibold text-white">Trending Manga</Text>
-            {mangaFetching && <Text className="text-white">Loading...</Text>}
+
+            {/* Popular Anime */}
+            {popularAnimeList && (
+               <Text className="mb-2 mt-6 text-xl font-semibold text-white">Popular Anime</Text>
+            )}
+            {popularAnimeList && (
+               <FlashList
+                  style={{ height: 350, width: "100%" }}
+                  data={popularAnimeList.Page.media}
+                  renderItem={({ item }) => (
+                     <TrendingMediaCard
+                        id={item.id}
+                        mediaType="ANIME"
+                        title={item.title.english}
+                        altTitle={item.title.romaji}
+                        score={item.averageScore}
+                        likes={item.favourites}
+                        coverImage={item.coverImage.large}
+                        bannerImage={item.bannerImage}
+                        description={item.description}
+                        genres={item.genres}
+                        secondText={item.studios.nodes[0]?.name || "Unknown Studio"}
+                        cardWidth={CARD_WIDTH}
+                     />
+                  )}
+                  horizontal
+               ></FlashList>
+            )}
+
+            {/* Trending Manga */}
+            {mangaList && (
+               <Text className="mb-2 mt-6 text-xl font-semibold text-white">Trending Manga</Text>
+            )}
             {mangaList && (
                <FlashList
                   style={{ height: 350, width: "100%" }}
                   data={mangaList.Page.media}
+                  horizontal
                   renderItem={({ item }) => (
                      <TrendingMediaCard
                         id={item.id}
@@ -83,9 +138,9 @@ const Index = () => {
                         cardWidth={CARD_WIDTH}
                      />
                   )}
-                  horizontal
                ></FlashList>
             )}
+            <View className="my-4"></View>
          </ScrollView>
       </View>
    );
