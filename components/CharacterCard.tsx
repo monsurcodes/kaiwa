8b635849bb5c95492fd2388a4055cc0c0d6f8861
monsurcodes <monsurcodes@gmail.com>
2026-04-17 20:34:36 +0530

    feat: add CharacterCard component for displaying character details

---
+++ components/CharacterCard.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Image, Pressable, Text, View } from "react-native";
+
+interface CharacterCardProps {
+   id: number;
+   name: string;
+   image: string;
+   role: string;
+}
+
+const CharacterCard = ({ id, name, image, role }: CharacterCardProps) => {
+   return (
+      <Pressable
+         style={{ width: 95, height: 165 }}
+         className="mr-2 overflow-hidden"
+         onPress={() => console.log(`Character ID: ${id}`)}
+      >
+         <Image source={{ uri: image }} className="h-[120] w-[95] rounded-md" />
+         <View className="flex-1 px-2 py-1">
+            <Text className="text-[13px] font-medium text-white">{name}</Text>
+            <Text className="text-[10px] font-medium text-gray-400">{role}</Text>
+         </View>
+      </Pressable>
+   );
+};
+
+export default CharacterCard;
