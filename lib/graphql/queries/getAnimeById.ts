38895936f2f08ade860bbb6d054642b55492e9a8
monsurcodes <monsurcodes@gmail.com>
2026-04-17 20:31:01 +0530

    feat: add GraphQL query and TypeScript interface for fetching anime details by ID

---
+++ lib/graphql/queries/getAnimeById.ts
@@ -0,0 +1,97 @@
+import { gql } from "urql";
+
+export const GetAnimeByIdQuery = gql`
+   query GetAnimeById($mediaId: Int) {
+      Media(id: $mediaId, type: ANIME) {
+         id
+         title {
+            english
+            romaji
+            native
+         }
+         bannerImage
+         coverImage {
+            large
+         }
+         description
+         averageScore
+         favourites
+         format
+         episodes
+         duration
+         genres
+         season
+         source
+         status
+         trailer {
+            id
+            site
+            thumbnail
+         }
+         startDate {
+            day
+            month
+            year
+         }
+         endDate {
+            day
+            month
+            year
+         }
+         characters(sort: ROLE) {
+            edges {
+               role
+               node {
+                  id
+                  name {
+                     full
+                  }
+                  image {
+                     large
+                  }
+               }
+            }
+         }
+         studios {
+            edges {
+               id
+               isMain
+               node {
+                  id
+                  name
+                  isAnimationStudio
+                  siteUrl
+               }
+            }
+         }
+         relations {
+            edges {
+               id
+               relationType
+               node {
+                  id
+                  type
+                  title {
+                     english
+                     romaji
+                  }
+                  coverImage {
+                     large
+                  }
+               }
+            }
+         }
+         nextAiringEpisode {
+            episode
+            airingAt
+            timeUntilAiring
+         }
+         tags {
+            id
+            name
+            description
+            isMediaSpoiler
+         }
+      }
+   }
+`;
---
+++ types/animeByIdInterface.ts
@@ -0,0 +1,95 @@
+export type AnimeByIdInterface = {
+   data: {
+      Media: {
+         id: number;
+         title: {
+            english: string;
+            romaji: string;
+            native: string;
+         };
+         bannerImage: string;
+         coverImage: {
+            large: string;
+         };
+         description: string;
+         averageScore: number;
+         favourites: number;
+         format: string;
+         episodes: number;
+         duration: number;
+         genres: string[];
+         season: string;
+         source: string;
+         status: string;
+         trailer: {
+            id: string;
+            site: string;
+            thumbnail: string;
+         };
+         startDate: {
+            day: number;
+            month: number;
+            year: number;
+         };
+         endDate: {
+            day: number;
+            month: number;
+            year: number;
+         };
+         characters: {
+            edges: {
+               role: string;
+               node: {
+                  id: number;
+                  name: {
+                     full: string;
+                  };
+                  image: {
+                     large: string;
+                  };
+               };
+            }[];
+         };
+         studios: {
+            edges: {
+               id: number;
+               isMain: boolean;
+               node: {
+                  id: number;
+                  name: string;
+                  isAnimationStudio: boolean;
+                  siteUrl: string;
+               };
+            }[];
+         };
+         relations: {
+            edges: {
+               id: number;
+               relationType: string;
+               node: {
+                  id: number;
+                  type: string;
+                  title: {
+                     english: string;
+                     romaji: string;
+                  };
+                  coverImage: {
+                     large: string;
+                  };
+               };
+            }[];
+         };
+         nextAiringEpisode: {
+            airingAt: number;
+            timeUntilAiring: number;
+            episode: number;
+         };
+         tags: {
+            id: number;
+            name: string;
+            description: string;
+            isMediaSpoiler: boolean;
+         }[];
+      };
+   };
+};
