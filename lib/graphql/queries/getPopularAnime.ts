9b24ceab5e781d73d4657709ccfbb1a2ebf5b55f
monsurcodes <monsurcodes@gmail.com>
2026-04-17 20:30:24 +0530

    feat: add GraphQL query and TypeScript interface for popular anime data

---
+++ lib/graphql/queries/getPopularAnime.ts
@@ -0,0 +1,33 @@
+import { gql } from "urql";
+
+export const GetPopularAnimeQuery = gql`
+   query GetPopularAnime($page: Int = 1) {
+      Page(page: $page, perPage: 10) {
+         pageInfo {
+            hasNextPage
+            currentPage
+         }
+         media(type: ANIME, sort: POPULARITY_DESC) {
+            id
+            title {
+               english
+               romaji
+            }
+            averageScore
+            favourites
+            coverImage {
+               large
+            }
+            bannerImage
+            description
+            genres
+            studios(isMain: true) {
+               nodes {
+                  id
+                  name
+               }
+            }
+         }
+      }
+   }
+`;
