aa71af1d48eefec52fececed55168fee8df33b13
monsurcodes <monsurcodes@gmail.com>
2026-04-20 21:54:49 +0530

    feat: add utility functions for converting minutes to days and getting month names

--- lib/utils/date.ts
+++ lib/utils/date.ts
@@ -9,3 +9,39 @@ export const formatAiringDate = (timestamp: number) => {
       hour12: true,
    }).format(new Date(timestamp * 1000));
 };
+
+export const minutesToDays = (minutes: number) => {
+   const days = minutes / 1440;
+   return days.toFixed(2);
+};
+
+export const getMonth = (month: number): string => {
+   switch (month) {
+      case 1:
+         return "Jan";
+      case 2:
+         return "Feb";
+      case 3:
+         return "Mar";
+      case 4:
+         return "Apr";
+      case 5:
+         return "May";
+      case 6:
+         return "Jun";
+      case 7:
+         return "Jul";
+      case 8:
+         return "Aug";
+      case 9:
+         return "Sep";
+      case 10:
+         return "Oct";
+      case 11:
+         return "Nov";
+      case 12:
+         return "Dec";
+      default:
+         return "";
+   }
+};
