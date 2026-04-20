5338b27311d098cbcf50d8349143ac94c2fe0685
monsurcodes <monsurcodes@gmail.com>
2026-04-20 21:58:26 +0530

    feat: enhance Profile component with user statistics and favorites display

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,13 @@
+import { FlashList } from "@shopify/flash-list";
 import { Image } from "expo-image";
 import React from "react";
 import { ActivityIndicator, ScrollView, Text, View } from "react-native";
 import { useQuery } from "urql";
 
+import FavCard from "@/components/FavCard";
 import MarkdownText from "@/components/MarkdownText";
 import { GetAuthUserDataQuery } from "@/lib/graphql/queries/getAuthUserData";
+import { minutesToDays } from "@/lib/utils/date";
 import { type AuthUserDataInterface } from "@/types/authUserDataInterface";
 
 const Profile = () => {
@@ -27,8 +30,12 @@ const Profile = () => {
 
    return (
       <View className="w-full flex-1">
-         <ScrollView className="flex-1" contentContainerStyle={{ alignItems: "center" }}>
-            {fetching && <Text className="text-white">Loading...</Text>}
+         <ScrollView
+            className="flex-1"
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+         >
+            {/* banner */}
             <View className="relative flex w-full items-center">
                {bannerUri ? (
                   <Image
@@ -74,15 +81,172 @@ const Profile = () => {
                   />
                )}
             </View>
-            <Text className="mt-20 text-xl font-semibold text-white">
-               Hello, {data?.Viewer.name}!
+
+            {/* greeting */}
+            <Text className="mx-auto mb-6 mt-[80px] text-xl font-semibold text-white">
+               {data?.Viewer.name}
             </Text>
 
-            {/* <Text className="text-white">{data?.Viewer.about}</Text> */}
-            {/* <HtmlText htmlContent={textAbout} /> */}
-            <MarkdownText content={data?.Viewer.about || ""} />
+            {/* about */}
+            <View className="mb-4">
+               <Text className="text-lg font-semibold text-white">About</Text>
+               <MarkdownText content={data?.Viewer.about || ""} />
+            </View>
 
-            <Text className="text-white"> Yo wassup</Text>
+            {/* stats */}
+            <View className="mb-4">
+               <Text className="mb-2 text-lg font-semibold text-white">Stats</Text>
+               <View className="gap-2 rounded-md bg-slate-900/70 px-4 py-2">
+                  {/* total anime */}
+                  <View className="flex-row">
+                     <Text className="w-1/2 text-white">Total Anime</Text>
+                     <Text className="w-1/2 text-white">{data?.Viewer.statistics.anime.count}</Text>
+                  </View>
+
+                  {/* ep watched */}
+                  <View className="flex-row">
+                     <Text className="w-1/2 text-white">Episodes Watched</Text>
+                     <Text className="w-1/2 text-white">
+                        {data?.Viewer.statistics.anime.episodesWatched}
+                     </Text>
+                  </View>
+
+                  {/* days watched */}
+                  <View className="flex-row">
+                     <Text className="w-1/2 text-white">Days Watched</Text>
+                     <Text className="w-1/2 text-white">
+                        {minutesToDays(data?.Viewer.statistics.anime.minutesWatched!)}
+                     </Text>
+                  </View>
+
+                  {/* anime mean score */}
+                  <View className="flex-row">
+                     <Text className="w-1/2 text-white">Anime Mean Score</Text>
+                     <Text className="w-1/2 text-white">
+                        {data?.Viewer.statistics.anime.meanScore}
+                     </Text>
+                  </View>
+
+                  {/* total manga */}
+                  <View className="flex-row">
+                     <Text className="w-1/2 text-white">Total Manga</Text>
+                     <Text className="w-1/2 text-white">{data?.Viewer.statistics.manga.count}</Text>
+                  </View>
+
+                  {/* chapters read */}
+                  <View className="flex-row">
+                     <Text className="w-1/2 text-white">Chapters Read</Text>
+                     <Text className="w-1/2 text-white">
+                        {data?.Viewer.statistics.manga.chaptersRead}
+                     </Text>
+                  </View>
+
+                  {/* manga mean score */}
+                  <View className="flex-row">
+                     <Text className="w-1/2 text-white">Manga Mean Score</Text>
+                     <Text className="w-1/2 text-white">
+                        {data?.Viewer.statistics.manga.meanScore}
+                     </Text>
+                  </View>
+               </View>
+            </View>
+
+            {/* fav anime */}
+            {data?.Viewer.favourites.anime.nodes.length! > 0 && (
+               <View className="mb-4">
+                  <Text className="mb-2 text-lg font-semibold text-white">Favorite Anime</Text>
+                  <FlashList
+                     data={data?.Viewer.favourites.anime.nodes}
+                     renderItem={({ item }) => (
+                        <FavCard
+                           id={item.id}
+                           title={item.title?.english || item.title?.romaji}
+                           image={item.coverImage?.large}
+                           type="anime"
+                        />
+                     )}
+                     keyExtractor={(item) => item.id.toString()}
+                     horizontal
+                  />
+               </View>
+            )}
+
+            {/* fav manga */}
+            {data?.Viewer.favourites.manga.nodes.length! > 0 && (
+               <View className="mb-4">
+                  <Text className="mb-2 text-lg font-semibold text-white">Favorite manga</Text>
+                  <FlashList
+                     data={data?.Viewer.favourites.manga.nodes}
+                     renderItem={({ item }) => (
+                        <FavCard
+                           id={item.id}
+                           title={item.title?.english || item.title?.romaji}
+                           image={item.coverImage?.extraLarge}
+                           type="manga"
+                        />
+                     )}
+                     keyExtractor={(item) => item.id.toString()}
+                     horizontal
+                  />
+               </View>
+            )}
+
+            {/* fav characters */}
+            {data?.Viewer.favourites.characters.nodes.length! > 0 && (
+               <View className="mb-4">
+                  <Text className="mb-2 text-lg font-semibold text-white">Favorite Characters</Text>
+                  <FlashList
+                     data={data?.Viewer.favourites.characters.nodes}
+                     renderItem={({ item }) => (
+                        <FavCard
+                           id={item.id}
+                           title={item.name.full}
+                           image={item.image.large}
+                           type="character"
+                        />
+                     )}
+                     keyExtractor={(item) => item.id.toString()}
+                     horizontal
+                  />
+               </View>
+            )}
+
+            {/* fav staff */}
+            {data?.Viewer.favourites.staff.nodes.length! > 0 && (
+               <View className="mb-4">
+                  <Text className="mb-2 text-lg font-semibold text-white">Favorite Staff</Text>
+                  <FlashList
+                     data={data?.Viewer.favourites.staff.nodes}
+                     renderItem={({ item }) => (
+                        <FavCard
+                           id={item.id}
+                           title={item.name.full}
+                           image={item.image.large}
+                           type="staff"
+                        />
+                     )}
+                     keyExtractor={(item) => item.id.toString()}
+                     horizontal
+                  />
+               </View>
+            )}
+
+            {/* fav studios */}
+            {data?.Viewer.favourites.studios.nodes.length! > 0 && (
+               <View className="mb-4">
+                  <Text className="mb-2 text-lg font-semibold text-white">Favorite Studios</Text>
+                  <FlashList
+                     data={data?.Viewer.favourites.studios.nodes}
+                     renderItem={({ item }) => (
+                        <Text className="mr-3 rounded-md bg-slate-900/70 px-3 py-2 font-bold text-white">
+                           {item.name}
+                        </Text>
+                     )}
+                     keyExtractor={(item) => item.id.toString()}
+                     horizontal
+                  />
+               </View>
+            )}
          </ScrollView>
       </View>
    );
