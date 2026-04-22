d95b3f5afe67d0ef86e6a8251e72125134efcb2f
monsurcodes <monsurcodes@gmail.com>
2026-04-23 03:01:41 +0530

    feat: add GetMangaById GraphQL query for fetching manga details

---
+++ lib/graphql/queries/getMangaById.ts
@@ -0,0 +1,92 @@
+import { gql } from "@/lib/graphql/generated";
+
+export const GetMangaByIdQuery = gql(/* GraphQL */ `
+   query GetMangaById($mediaId: Int) {
+      Media(id: $mediaId, type: MANGA) {
+         id
+         title {
+            english
+            romaji
+            native
+         }
+         bannerImage
+         coverImage {
+            extraLarge
+         }
+         description
+         averageScore
+         favourites
+         popularity
+         format
+         chapters
+         genres
+         source
+         status
+         trailer {
+            id
+            site
+            thumbnail
+         }
+         startDate {
+            day
+            month
+            year
+         }
+         endDate {
+            day
+            month
+            year
+         }
+         relations {
+            edges {
+               id
+               relationType
+               node {
+                  id
+                  title {
+                     english
+                     romaji
+                  }
+                  coverImage {
+                     large
+                  }
+                  type
+                  format
+               }
+            }
+         }
+         tags {
+            id
+            name
+            rank
+            description
+            isMediaSpoiler
+         }
+         recommendations {
+            nodes {
+               mediaRecommendation {
+                  id
+                  type
+                  title {
+                     english
+                     romaji
+                  }
+                  coverImage {
+                     large
+                  }
+               }
+            }
+         }
+         externalLinks {
+            color
+            icon
+            id
+            site
+            siteId
+            type
+            url
+            language
+         }
+      }
+   }
+`);
