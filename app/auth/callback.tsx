d41b321256a9e71180d427cb783a65022490057e
monsurcodes <monsurcodes@gmail.com>
2026-04-22 23:09:59 +0530

    refactor: remove unused useEffect hook from AuthCallback component

--- app/auth/callback.tsx
+++ app/auth/callback.tsx
@@ -1,9 +1,6 @@
-import { useEffect } from "react";
 import { ActivityIndicator, View } from "react-native";
 
 export default function AuthCallback() {
-   useEffect(() => {}, []);
-
    return (
       <View style={{ flex: 1, backgroundColor: "#030014", justifyContent: "center" }}>
          <ActivityIndicator size={30} />
