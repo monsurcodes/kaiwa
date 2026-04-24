67023cb58bc501a6b20bc66787751daebbab9c86
Monsur Mazumder <monsurcodes@gmail.com>
2026-04-24 21:20:51 +0530

    feat: add useAnimeDetail hook for querying anime details by media ID

---
+++ hooks/useAnimeDetail.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "urql";
+
+import { GetAnimeByIdQuery } from "@/lib/graphql/queries/getAnimeById";
+
+export const useAnimeDetail = (mediaId: number | null) => {
+   // query animes
+   const [results] = useQuery({
+      query: GetAnimeByIdQuery,
+      variables: {
+         mediaId: mediaId ?? 0,
+      },
+      pause: mediaId === null,
+   });
+
+   // extract animes from query results
+   const { data, fetching, error } = results;
+
+   return { data, fetching, error };
+};
