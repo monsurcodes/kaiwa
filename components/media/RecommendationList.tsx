a05933925b5e93cf69f0e3cda0157ce5b69fd8cf
Monsur Mazumder <monsurcodes@gmail.com>
2026-04-24 23:12:21 +0530

    feat: implement RecommendationCard component and update import path in RecommendationList

--- components/media/RecommendationList.tsx
+++ components/media/RecommendationList.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 
 import { SharedMedia } from "@/types";
 
-import RecommendationCard from "../RecommendationCard";
+import RecommendationCard from "./Cards/RecommendationCard";
 
 interface RecommendationListProps {
    media: SharedMedia | null | undefined;
