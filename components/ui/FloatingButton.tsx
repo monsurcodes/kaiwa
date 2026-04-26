289f0bba6e4cdc9d1b2a68139f10af4ff9033fb6
Monsur Mazumder <monsurcodes@gmail.com>
2026-04-26 17:12:58 +0530

    feat: enhance FloatingButton component with optional positioning props

--- components/ui/FloatingButton.tsx
+++ components/ui/FloatingButton.tsx
@@ -1,18 +1,28 @@
 import { LucideIcon } from "lucide-react-native";
 import { StyleSheet, TouchableOpacity } from "react-native";
 
+import { theme } from "@/constants/theme";
+
 interface FloatingButtonProps {
    Icon: LucideIcon;
    strokeWidth?: number;
-   onPress: () => void;
+   onPress?: () => void;
+   bottomPos?: number;
+   rightPos?: number;
 }
 
-const FloatingButton = ({ Icon, strokeWidth, onPress }: FloatingButtonProps) => {
+const FloatingButton = ({
+   Icon,
+   strokeWidth,
+   onPress,
+   bottomPos,
+   rightPos,
+}: FloatingButtonProps) => {
    return (
       <TouchableOpacity
          activeOpacity={0.8}
          onPress={onPress}
-         style={styles.button}
+         style={[styles.button, { bottom: bottomPos ?? 24, right: rightPos ?? 24 }]}
          className="items-center justify-center rounded-lg shadow-lg shadow-slate-500/50"
       >
          <Icon color="#CCD0CF" size={32} strokeWidth={strokeWidth} />
@@ -23,13 +33,11 @@ const FloatingButton = ({ Icon, strokeWidth, onPress }: FloatingButtonProps) => 
 const styles = StyleSheet.create({
    button: {
       position: "absolute",
-      bottom: 24,
-      right: 24,
       zIndex: 50,
       width: 45,
       height: 45,
       elevation: 8,
-      backgroundColor: "#11212D",
+      backgroundColor: theme.bg.overlay,
    },
 });
 
